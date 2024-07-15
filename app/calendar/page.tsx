6b6adddaf88cb53825b162f294b172e4354460ed
eeekeee <ekrtionl@gmail.com>
2024-07-16 00:44:46 +0900

    feat: calendar 일정 목록 불러오기 및 캘린더 표시 구현

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,16 +1,76 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "../../assets/custom-calendar.css";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
+type Todo = {
+  title: string;
+  date: Date;
+};
 
 export default function CalendarPage() {
-  const today = new Date();
+  const offset = new Date().getTimezoneOffset() * 60000;
+  const today = new Date(Date.now() - offset);
   const [date, setDate] = useState<Value>(today);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const sampleTodos = [
+    {
+      title: "놀기",
+      date: new Date("2024-07-12"),
+    },
+    {
+      title: "놀기",
+      date: new Date("2024-07-15"),
+    },
+    {
+      title: "놀기2xxxxxxxxxxxxxxxxx",
+      date: new Date("2024-07-15"),
+    },
+    {
+      title: "놀기3",
+      date: new Date("2024-07-15"),
+    },
+    {
+      title: "공부하기 16",
+      date: new Date("2024-07-16"),
+    },
+    {
+      title: "운동하기 17",
+      date: new Date("2024-07-17"),
+    },
+    {
+      title: "운동하기 29",
+      date: new Date("2024-07-29"),
+    },
+    {
+      title: "운동하기 8-2",
+      date: new Date("2024-08-2"),
+    },
+  ];
+
+  useEffect(() => {
+    if (date instanceof Date) {
+      const selectedDate = formattedDate(date);
+      const filteredTodos = sampleTodos.filter(
+        (todo) => formattedDate(todo.date) === selectedDate
+      );
+      setTodos(filteredTodos);
+    } else {
+      setTodos([]);
+    }
+  }, [date]);
+
+  const formattedDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
 
   const dateChangeHandler = (newDate: Value) => {
     setDate(newDate);
@@ -25,14 +85,25 @@ export default function CalendarPage() {
   };
 
   const addContent = ({ date }: { date: Date }) => {
+    let content = [];
     if (isSameDay(date, today)) {
-      return (
-        <p>
+      content.push(
+        <p key={`today-${date.getDate()}`}>
           Today {date.getMonth() + 1}/{date.getDate()}
         </p>
       );
+    } else {
+      content.push(<br key={`break-${date.getDate()}`} />);
     }
-    return null;
+    const todosForDate = sampleTodos.filter((todo) =>
+      isSameDay(todo.date, date)
+    );
+
+    for (let i = 0; i < todosForDate.length; i++) {
+      content.push(<span key={`todo-${i}-${date.getDate()}`}>✨</span>);
+    }
+
+    return <>{content}</>;
   };
 
   return (
@@ -56,6 +127,11 @@ export default function CalendarPage() {
               day: "2-digit",
             })}
           </p>
+          {todos.map((todo, index) => (
+            <p key={index} className="border-4 py-4">
+              {todo.title}
+            </p>
+          ))}
         </div>
       </div>
     </main>
